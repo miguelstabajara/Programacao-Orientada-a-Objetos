c9b0fc6fd035cf3540de0cc8ffb768d134022951
Miguel Soto Tabajara <94875354+miguelstabajara@users.noreply.github.com>
2024-02-15 11:34:55 -0300

    Add files via upload

---
+++ Prática 4/src/Teste.java
@@ -0,0 +1,12 @@
+
+public class Teste {
+
+	public static void main(String[] args) {
+		Onibus um = new Onibus("AEB1111",1998,40);
+		um.exibirDados();
+		Caminhao dois = new Caminhao("ABC0001",2005,4);
+		dois.exibirDados();
+	}
+	/*Neste exercício estamos usando os conceitos de construtores, hierarquias e sobreposições. */
+
+}
